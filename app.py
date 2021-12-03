7d504c316c2d345c2a758dceaee3076aaaefa388
Stefan Holmberg <stefan.holmberg@systementor.se>
2021-12-03 10:58:40 +0100

    barnum

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, upgrade
+import barnum
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:password@localhost/databas20210203'
@@ -11,6 +12,7 @@ class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     namn = db.Column(db.String(100), unique=False, nullable=False)
     city = db.Column(db.String(80), unique=False, nullable=False)
+    phone = db.Column(db.String(20), unique=False, nullable=False)
     postalcode = db.Column(db.String(10), unique=False, nullable=False)
     cards = db.relationship('Creditcard', backref='person', lazy=True)
 
@@ -24,8 +26,30 @@ class Creditcard(db.Model):
 if __name__ == "__main__":
     with app.app_context():
         upgrade()
+    print("2. Skapa ny fejkperson")
     print("1. Skapa ny person")
     sel= input("val:")
+    if sel == "2":
+        p = Person()
+        p.phone = barnum.create_phone()
+        n = barnum.create_name()
+        p.namn = n[0] + " " + n[1]
+        n = barnum.create_city_state_zip()
+        p.city = n[1]
+        p.postalcode = n[0]
+        c = Creditcard()
+        cn = barnum.create_cc_number()
+        c.typ = cn[0]
+        c.cardnumber = cn[1][0]
+        p.cards.append(c)
+        c = Creditcard()
+        cn = barnum.create_cc_number()
+        c.typ = cn[0]
+        c.cardnumber = cn[1][0]
+        p.cards.append(c)
+
+        db.session.add(p)
+        db.session.commit()
     if sel == "1":
         p = Person()
         p.namn = input("Ange namn:")
